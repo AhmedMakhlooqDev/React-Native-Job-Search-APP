8c9783ab0fc2c56eb1c58695ab02d76ccdb7192f
AhmedMakhlooqDev <ahmedmakhlooqdev@gmail.com>
2023-10-01 17:40:52 +0300

    initials

---
+++ app/index.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, userRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBttn, Welcome } from '../components';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
